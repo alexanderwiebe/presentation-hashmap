07e78f9bd2f568089a8e7d76dd818cc13266f6e4
Alexander Wiebe <alexanderjwiebe@gmail.com>
2022-04-26 20:01:35 -0600

    updated the pointer content

--- src/app/pointer-demo/pointer-demo.component.ts
+++ src/app/pointer-demo/pointer-demo.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import sdk from '@stackblitz/sdk';
 @Component({
   selector: 'app-pointer-demo',
   templateUrl: './pointer-demo.component.html',
-  styleUrls: ['./pointer-demo.component.scss']
+  styleUrls: ['./pointer-demo.component.scss'],
 })
 export class PointerDemoComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    sdk.embedProjectId('sb-pointer-demo', 'typescript-jfauku', {
+      height: '100%',
+      forceEmbedLayout: true,
+      openFile: 'index.ts',
+      devToolsHeight: 100,
+    });
   }
-
 }
--- src/app/pointer-exp/pointer-exp.component.ts
+++ src/app/pointer-exp/pointer-exp.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pointer-exp',
   templateUrl: './pointer-exp.component.html',
-  styleUrls: ['./pointer-exp.component.scss']
+  styleUrls: ['./pointer-exp.component.scss'],
 })
 export class PointerExpComponent implements OnInit {
+  markdown = `\`\`\` javascript
+  let myName = "AJ";
+  // myName has some memory location eg: 0x12345678
+  let myObj = { name: "AJ", favColour: "blue" };
+  // myObj has some memory location eg: 0x87654321
+  // when we operate with:
+  //     myName its using the value not the reference
+  //     myObj its using the reference not the value
+  \`\`\``;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
